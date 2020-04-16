19b99a76796fbe7d0dc849b59d39faa29eacb87c
daelynj <daelyndbjones@gmail.com>
2020-04-16 15:30:17 -0700

    Create mock data
    items.ts shows the data that we will receive from the API.
    
    buildData shows the data that we want to coerce the data from items.ts
    into.

---
+++ src/components/app/data/buildData.ts
@@ -0,0 +1,20 @@
+export function buildData(items: any) {
+  return [
+    {
+      item_id: 'T5_BAG',
+      city: 'Bridgewatch',
+      quality: 1,
+      sell_price_min: 3499,
+      black_market_buy_price_min: 5796,
+      profit: 2297,
+    },
+    {
+      item_id: 'T4_BAG',
+      city: 'Bridgewatch',
+      quality: 1,
+      sell_price_min: 3499,
+      black_market_buy_price_min: 808,
+      profit: -2691,
+    },
+  ]
+}
