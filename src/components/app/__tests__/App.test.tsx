26690d59ef03868d22af5c115ef169dd502ade53
daelynj <daelyndbjones@gmail.com>
2020-04-15 19:53:59 -0700

    Fix test

--- src/components/app/__tests__/App.test.tsx
+++ src/components/app/__tests__/App.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import App from '../app'
 
 describe('App', () => {
-  it('does nothing', () => {
-    console.log('nothing')
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
   })
 })
