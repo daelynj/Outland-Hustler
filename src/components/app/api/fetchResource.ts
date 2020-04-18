0f7dff933e9ffc52399602e40e8907c525c4e3b9
daelynj <daelyndbjones@gmail.com>
2020-04-18 01:42:52 -0700

    Create API wrapper

---
+++ src/components/app/api/fetchResource.ts
@@ -0,0 +1,20 @@
+export const fetchResource = (url: any, path: any) => {
+  url =
+    'https://www.albion-online-data.com/api/v2/stats/prices/T4_BAG,T4_BAG@1,T5_BAG,T5_BAG@1,T6_BAG,T6_BAG@1?locations=Black%20Market,Bridgewatch,Caerleon,Fort%20Sterling,%20Martlock,%20Lymhurst&qualities=1'
+  let response: any = null
+
+  return fetch(url)
+    .then((responseObject) => {
+      response = responseObject
+
+      if (response.status < 200 || response.status >= 300)
+        return response.text()
+
+      return response.json()
+    })
+    .then((parsedResponse) => {
+      if (response.status < 200 || response.status >= 300) throw parsedResponse
+
+      return parsedResponse
+    })
+}
--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './css/app.css'
 import ItemContainer from '../item/itemContainer'
 import RefreshButton from '../refresh/refreshButton'
-import { items } from './api/items'
+import { fetchResource } from './api/fetchResource'
 import { buildData } from './data/buildData'
 
 const App: React.FC = () => {
+  const [items, setItems] = useState([])
+
   const refreshPage = () => {
     window.location.reload(false)
   }
+
+  useEffect(() => {
+    fetchResource('no url yet', 'no path yet')
+      .then((userData) => {
+        setItems(userData)
+      })
+      .catch((error) => {
+        console.log(error.message)
+        console.log(error.status)
+        console.log(error.response)
+      })
+  })
 
   return (
     <>
