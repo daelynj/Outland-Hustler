986ffd6f079311e8b799b5f804aeeb25937b7d11
daelynj <daelyndbjones@gmail.com>
2020-04-26 14:15:24 -0700

    Fix CORS issue by adding a proxy

--- src/components/app/api/gameInfoClient.ts
+++ src/components/app/api/gameInfoClient.ts
@@ -2,13 +2,15 @@ import { fetchResource } from './fetchResource'
 
 export class GameInfoClient {
   BASE_URL = 'https://gameinfo.albiononline.com/api/gameinfo/'
+  PROXY_URL = 'https://cors-anywhere.herokuapp.com/'
 
   _getItemData = (item: String) => fetchResource(this.buildItemDataURL(item))
 
   _getItemPicture = (item: String) =>
     fetchResource(this.buildItemPictureURL(item))
 
-  buildItemDataURL = (item: String) => this.BASE_URL + 'items/' + item + '/data'
+  buildItemDataURL = (item: String) =>
+    this.PROXY_URL + this.BASE_URL + 'items/' + item + '/data'
 
-  buildItemPictureURL = (item: String) => this.BASE_URL + item
+  buildItemPictureURL = (item: String) => this.PROXY_URL + this.BASE_URL + item
 }
