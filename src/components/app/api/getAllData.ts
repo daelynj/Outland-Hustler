1a1d68d02d36fdc57ec543e84301c8567e7500b2
daelynj <daelyndbjones@gmail.com>
2020-04-26 16:26:51 -0700

    Separate API calls into a testable file

---
+++ src/components/app/api/getAllData.ts
@@ -0,0 +1,43 @@
+import { buildData } from '../data/buildData'
+import { PriceClient } from './priceClient'
+import { GameInfoClient } from './gameInfoClient'
+
+export const getAllData = (
+  setOrganizedItems: any,
+  items: string[],
+  qualities: number[],
+  cities: string[]
+) => {
+  let priceClient = new PriceClient()
+  let gameInfoClient = new GameInfoClient()
+  let itemNames = new Map()
+  let itemData: string[] = []
+
+  if (items.length > 0 && cities.length > 0 && qualities.length > 0) {
+    priceClient
+      ._getPriceData(items, ['Black Market'].concat(cities), qualities)
+      .then((itemDataResponse) => {
+        itemData = itemDataResponse
+
+        items.forEach(function (item: any) {
+          gameInfoClient
+            ._getItemData(item)
+            .then((nameDataResponse) => {
+              itemNames.set(
+                nameDataResponse.uniqueName,
+                nameDataResponse.localizedNames['EN-US']
+              )
+            })
+            .then(() => {
+              setOrganizedItems(buildData(itemData, itemNames))
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+}
