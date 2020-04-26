30ed292c97d4138ba0bd2578bc3f366ec906acdd
daelynj <daelyndbjones@gmail.com>
2020-04-26 16:27:19 -0700

    Use separated method for API calls

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,13 +3,9 @@ import './css/app.css'
 import ItemContainer from '../item/itemContainer'
 import RefreshButton from '../refresh/refreshButton'
 import ChoiceContainer from '../choice/choiceContainer'
-import { PriceClient } from './api/priceClient'
-import { buildData } from './data/buildData'
-import { GameInfoClient } from './api/gameInfoClient'
+import { getAllData } from './api/getAllData'
 
 const App: React.FC = () => {
-  const [priceClient] = useState(new PriceClient())
-  const [gameInfoClient] = useState(new GameInfoClient())
   const [organizedItems, setOrganizedItems] = useState(new Map())
   const [items, setItems] = useState([])
   const [cities, setCities] = useState([])
@@ -20,37 +16,8 @@ const App: React.FC = () => {
   }
 
   useEffect(() => {
-    let itemNames = new Map()
-    let itemData: String[] = []
-
-    if (items.length > 0 && cities.length > 0 && qualities.length > 0) {
-      priceClient
-        ._getPriceData(items, ['Black Market'].concat(cities), qualities)
-        .then((itemDataResponse) => {
-          itemData = itemDataResponse
-
-          items.forEach(function (item: any) {
-            gameInfoClient
-              ._getItemData(item)
-              .then((nameDataResponse) => {
-                itemNames.set(
-                  nameDataResponse.uniqueName,
-                  nameDataResponse.localizedNames['EN-US']
-                )
-              })
-              .then(() => {
-                setOrganizedItems(buildData(itemData, itemNames))
-              })
-              .catch((error) => {
-                console.log(error)
-              })
-          })
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    }
-  }, [priceClient, gameInfoClient, items, qualities, cities])
+    getAllData(setOrganizedItems, items, qualities, cities)
+  }, [items, qualities, cities])
 
   const completeEntriesExist = () =>
     items.length > 0 && cities.length > 0 && qualities.length > 0
