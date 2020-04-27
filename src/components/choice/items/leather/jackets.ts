02e67772b9a641d92e72cf3e5106788530db5f81
daelynj <daelyndbjones@gmail.com>
2020-04-27 15:59:53 -0700

    Add leather jackets

---
+++ src/components/choice/items/leather/jackets.ts
@@ -0,0 +1,63 @@
+export const jackets = [
+  {
+    key: 'T4_ARMOR_LEATHER_SET1',
+    text: "Adept's Mercenary Jacket",
+    value: 'T4_ARMOR_LEATHER_SET1',
+  },
+  {
+    key: 'T5_ARMOR_LEATHER_SET1',
+    text: "Expert's Mercenary Jacket",
+    value: 'T5_ARMOR_LEATHER_SET1',
+  },
+  {
+    key: 'T6_ARMOR_LEATHER_SET1',
+    text: "Master's Mercenary Jacket",
+    value: 'T6_ARMOR_LEATHER_SET1',
+  },
+  {
+    key: 'T7_ARMOR_LEATHER_SET1',
+    text: "Grandmaster's Mercenary Jacket",
+    value: 'T7_ARMOR_LEATHER_SET1',
+  },
+
+  {
+    key: 'T4_ARMOR_LEATHER_SET2',
+    text: "Adept's Hunter Jacket",
+    value: 'T4_ARMOR_LEATHER_SET2',
+  },
+  {
+    key: 'T5_ARMOR_LEATHER_SET2',
+    text: "Expert's Hunter Jacket",
+    value: 'T5_ARMOR_LEATHER_SET2',
+  },
+  {
+    key: 'T6_ARMOR_LEATHER_SET2',
+    text: "Master's Hunter Jacket",
+    value: 'T6_ARMOR_LEATHER_SET2',
+  },
+  {
+    key: 'T7_ARMOR_LEATHER_SET2',
+    text: "Grandmaster's Hunter Jacket",
+    value: 'T7_ARMOR_LEATHER_SET2',
+  },
+  {
+    key: 'T4_ARMOR_LEATHER_SET3',
+    text: "Adept's Assassin Jacket",
+    value: 'T4_ARMOR_LEATHER_SET3',
+  },
+  {
+    key: 'T5_ARMOR_LEATHER_SET3',
+    text: "Expert's Assassin Jacket",
+    value: 'T5_ARMOR_LEATHER_SET3',
+  },
+  {
+    key: 'T6_ARMOR_LEATHER_SET3',
+    text: "Master's Assassin Jacket",
+    value: 'T6_ARMOR_LEATHER_SET3',
+  },
+  {
+    key: 'T7_ARMOR_LEATHER_SET3',
+    text: "Grandmaster's Assassin Jacket",
+    value: 'T7_ARMOR_LEATHER_SET3',
+  },
+]
