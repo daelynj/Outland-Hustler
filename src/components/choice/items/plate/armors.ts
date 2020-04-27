e36b422654ec1d7b31eeaea0c93e99fd21940b61
daelynj <daelyndbjones@gmail.com>
2020-04-27 15:59:19 -0700

    Add plate armors

---
+++ src/components/choice/items/plate/armors.ts
@@ -0,0 +1,63 @@
+export const armors = [
+  {
+    key: 'T4_ARMOR_PLATE_SET1',
+    text: "Adept's Soldier Armor",
+    value: 'T4_ARMOR_PLATE_SET1',
+  },
+  {
+    key: 'T5_ARMOR_PLATE_SET1',
+    text: "Expert's Soldier Armor",
+    value: 'T5_ARMOR_PLATE_SET1',
+  },
+  {
+    key: 'T6_ARMOR_PLATE_SET1',
+    text: "Master's Soldier Armor",
+    value: 'T6_ARMOR_PLATE_SET1',
+  },
+  {
+    key: 'T7_ARMOR_PLATE_SET1',
+    text: "Grandmaster's Soldier Armor",
+    value: 'T7_ARMOR_PLATE_SET1',
+  },
+
+  {
+    key: 'T4_ARMOR_PLATE_SET2',
+    text: "Adept's Knight Armor",
+    value: 'T4_ARMOR_PLATE_SET2',
+  },
+  {
+    key: 'T5_ARMOR_PLATE_SET2',
+    text: "Expert's Knight Armor",
+    value: 'T5_ARMOR_PLATE_SET2',
+  },
+  {
+    key: 'T6_ARMOR_PLATE_SET2',
+    text: "Master's Knight Armor",
+    value: 'T6_ARMOR_PLATE_SET2',
+  },
+  {
+    key: 'T7_ARMOR_PLATE_SET2',
+    text: "Grandmaster's Knight Armor",
+    value: 'T7_ARMOR_PLATE_SET2',
+  },
+  {
+    key: 'T4_ARMOR_PLATE_SET3',
+    text: "Adept's Guardian Armor",
+    value: 'T4_ARMOR_PLATE_SET3',
+  },
+  {
+    key: 'T5_ARMOR_PLATE_SET3',
+    text: "Expert's Guardian Armor",
+    value: 'T5_ARMOR_PLATE_SET3',
+  },
+  {
+    key: 'T6_ARMOR_PLATE_SET3',
+    text: "Master's Guardian Armor",
+    value: 'T6_ARMOR_PLATE_SET3',
+  },
+  {
+    key: 'T7_ARMOR_PLATE_SET3',
+    text: "Grandmaster's Guardian Armor",
+    value: 'T7_ARMOR_PLATE_SET3',
+  },
+]
