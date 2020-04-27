6e0f7f8f28580bff8d4c36aa7e6ee61ef0841f18
daelynj <daelyndbjones@gmail.com>
2020-04-27 16:00:44 -0700

    Add cloth cowls

---
+++ src/components/choice/items/cloth/cowls.ts
@@ -0,0 +1,63 @@
+export const cowls = [
+  {
+    key: 'T4_HEAD_CLOTH_SET1',
+    text: "Adept's Scholar Cowl",
+    value: 'T4_HEAD_CLOTH_SET1',
+  },
+  {
+    key: 'T5_HEAD_CLOTH_SET1',
+    text: "Expert's Scholar Cowl",
+    value: 'T5_HEAD_CLOTH_SET1',
+  },
+  {
+    key: 'T6_HEAD_CLOTH_SET1',
+    text: "Master's Scholar Cowl",
+    value: 'T6_HEAD_CLOTH_SET1',
+  },
+  {
+    key: 'T7_HEAD_CLOTH_SET1',
+    text: "Grandmaster's Scholar Cowl",
+    value: 'T7_HEAD_CLOTH_SET1',
+  },
+
+  {
+    key: 'T4_HEAD_CLOTH_SET2',
+    text: "Adept's Cleric Cowl",
+    value: 'T4_HEAD_CLOTH_SET2',
+  },
+  {
+    key: 'T5_HEAD_CLOTH_SET2',
+    text: "Expert's Cleric Cowl",
+    value: 'T5_HEAD_CLOTH_SET2',
+  },
+  {
+    key: 'T6_HEAD_CLOTH_SET2',
+    text: "Master's Cleric Cowl",
+    value: 'T6_HEAD_CLOTH_SET2',
+  },
+  {
+    key: 'T7_HEAD_CLOTH_SET2',
+    text: "Grandmaster's Cleric Cowl",
+    value: 'T7_HEAD_CLOTH_SET2',
+  },
+  {
+    key: 'T4_HEAD_CLOTH_SET3',
+    text: "Adept's Mage Cowl",
+    value: 'T4_HEAD_CLOTH_SET3',
+  },
+  {
+    key: 'T5_HEAD_CLOTH_SET3',
+    text: "Expert's Mage Cowl",
+    value: 'T5_HEAD_CLOTH_SET3',
+  },
+  {
+    key: 'T6_HEAD_CLOTH_SET3',
+    text: "Master's Mage Cowl",
+    value: 'T6_HEAD_CLOTH_SET3',
+  },
+  {
+    key: 'T7_HEAD_CLOTH_SET3',
+    text: "Grandmaster's Mage Cowl",
+    value: 'T7_HEAD_CLOTH_SET3',
+  },
+]
