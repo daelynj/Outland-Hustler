8dcc0f7527cbc04a5fde43711145c8b9e5e40ace
daelynj <daelyndbjones@gmail.com>
2020-04-27 16:00:35 -0700

    Add cloth robes

---
+++ src/components/choice/items/cloth/robes.ts
@@ -0,0 +1,63 @@
+export const robes = [
+  {
+    key: 'T4_ARMOR_CLOTH_SET1',
+    text: "Adept's Scholar Robe",
+    value: 'T4_ARMOR_CLOTH_SET1',
+  },
+  {
+    key: 'T5_ARMOR_CLOTH_SET1',
+    text: "Expert's Scholar Robe",
+    value: 'T5_ARMOR_CLOTH_SET1',
+  },
+  {
+    key: 'T6_ARMOR_CLOTH_SET1',
+    text: "Master's Scholar Robe",
+    value: 'T6_ARMOR_CLOTH_SET1',
+  },
+  {
+    key: 'T7_ARMOR_CLOTH_SET1',
+    text: "Grandmaster's Scholar Robe",
+    value: 'T7_ARMOR_CLOTH_SET1',
+  },
+
+  {
+    key: 'T4_ARMOR_CLOTH_SET2',
+    text: "Adept's Cleric Robe",
+    value: 'T4_ARMOR_CLOTH_SET2',
+  },
+  {
+    key: 'T5_ARMOR_CLOTH_SET2',
+    text: "Expert's Cleric Robe",
+    value: 'T5_ARMOR_CLOTH_SET2',
+  },
+  {
+    key: 'T6_ARMOR_CLOTH_SET2',
+    text: "Master's Cleric Robe",
+    value: 'T6_ARMOR_CLOTH_SET2',
+  },
+  {
+    key: 'T7_ARMOR_CLOTH_SET2',
+    text: "Grandmaster's Cleric Robe",
+    value: 'T7_ARMOR_CLOTH_SET2',
+  },
+  {
+    key: 'T4_ARMOR_CLOTH_SET3',
+    text: "Adept's Mage Robe",
+    value: 'T4_ARMOR_CLOTH_SET3',
+  },
+  {
+    key: 'T5_ARMOR_CLOTH_SET3',
+    text: "Expert's Mage Robe",
+    value: 'T5_ARMOR_CLOTH_SET3',
+  },
+  {
+    key: 'T6_ARMOR_CLOTH_SET3',
+    text: "Master's Mage Robe",
+    value: 'T6_ARMOR_CLOTH_SET3',
+  },
+  {
+    key: 'T7_ARMOR_CLOTH_SET3',
+    text: "Grandmaster's Mage Robe",
+    value: 'T7_ARMOR_CLOTH_SET3',
+  },
+]
