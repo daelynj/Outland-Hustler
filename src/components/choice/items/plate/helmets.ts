15449f45d6efc209c36cdfd5f3b101e19bed8315
daelynj <daelyndbjones@gmail.com>
2020-04-27 15:58:52 -0700

    Add plate helmets

---
+++ src/components/choice/items/plate/helmets.ts
@@ -0,0 +1,63 @@
+export const helmets = [
+  {
+    key: 'T4_HEAD_PLATE_SET1',
+    text: "Adept's Soldier Helmet",
+    value: 'T4_HEAD_PLATE_SET1',
+  },
+  {
+    key: 'T5_HEAD_PLATE_SET1',
+    text: "Expert's Soldier Helmet",
+    value: 'T5_HEAD_PLATE_SET1',
+  },
+  {
+    key: 'T6_HEAD_PLATE_SET1',
+    text: "Master's Soldier Helmet",
+    value: 'T6_HEAD_PLATE_SET1',
+  },
+  {
+    key: 'T7_HEAD_PLATE_SET1',
+    text: "Grandmaster's Soldier Helmet",
+    value: 'T7_HEAD_PLATE_SET1',
+  },
+
+  {
+    key: 'T4_HEAD_PLATE_SET2',
+    text: "Adept's Knight Helmet",
+    value: 'T4_HEAD_PLATE_SET2',
+  },
+  {
+    key: 'T5_HEAD_PLATE_SET2',
+    text: "Expert's Knight Helmet",
+    value: 'T5_HEAD_PLATE_SET2',
+  },
+  {
+    key: 'T6_HEAD_PLATE_SET2',
+    text: "Master's Knight Helmet",
+    value: 'T6_HEAD_PLATE_SET2',
+  },
+  {
+    key: 'T7_HEAD_PLATE_SET2',
+    text: "Grandmaster's Knight Helmet",
+    value: 'T7_HEAD_PLATE_SET2',
+  },
+  {
+    key: 'T4_HEAD_PLATE_SET3',
+    text: "Adept's Guardian Helmet",
+    value: 'T4_HEAD_PLATE_SET3',
+  },
+  {
+    key: 'T5_HEAD_PLATE_SET3',
+    text: "Expert's Guardian Helmet",
+    value: 'T5_HEAD_PLATE_SET3',
+  },
+  {
+    key: 'T6_HEAD_PLATE_SET3',
+    text: "Master's Guardian Helmet",
+    value: 'T6_HEAD_PLATE_SET3',
+  },
+  {
+    key: 'T7_HEAD_PLATE_SET3',
+    text: "Grandmaster's Guardian Helmet",
+    value: 'T7_HEAD_PLATE_SET3',
+  },
+]
