5b2d44dd8b727ff65da2d97e0f0e88c376e644d2
daelynj <daelyndbjones@gmail.com>
2020-04-27 16:01:26 -0700

    Add leather items grouping

---
+++ src/components/choice/items/itemGroups/leatherItems.ts
@@ -0,0 +1,5 @@
+import { hoods } from '../leather/hoods'
+import { jackets } from '../leather/jackets'
+import { shoes } from '../leather/shoes'
+
+export const leatherItems = hoods.concat(jackets, shoes)
