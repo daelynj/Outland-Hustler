69421ca27ace3a5676e66af8a11d0b0d2a8ccccb
daelynj <daelyndbjones@gmail.com>
2020-04-27 15:59:05 -0700

    Add plate boots

---
+++ src/components/choice/items/plate/boots.ts
@@ -0,0 +1,63 @@
+export const boots = [
+  {
+    key: 'T4_SHOES_PLATE_SET1',
+    text: "Adept's Soldier Shoes",
+    value: 'T4_SHOES_PLATE_SET1',
+  },
+  {
+    key: 'T5_SHOES_PLATE_SET1',
+    text: "Expert's Soldier Shoes",
+    value: 'T5_SHOES_PLATE_SET1',
+  },
+  {
+    key: 'T6_SHOES_PLATE_SET1',
+    text: "Master's Soldier Shoes",
+    value: 'T6_SHOES_PLATE_SET1',
+  },
+  {
+    key: 'T7_SHOES_PLATE_SET1',
+    text: "Grandmaster's Soldier Shoes",
+    value: 'T7_SHOES_PLATE_SET1',
+  },
+
+  {
+    key: 'T4_SHOES_PLATE_SET2',
+    text: "Adept's Knight Shoes",
+    value: 'T4_SHOES_PLATE_SET2',
+  },
+  {
+    key: 'T5_SHOES_PLATE_SET2',
+    text: "Expert's Knight Shoes",
+    value: 'T5_SHOES_PLATE_SET2',
+  },
+  {
+    key: 'T6_SHOES_PLATE_SET2',
+    text: "Master's Knight Shoes",
+    value: 'T6_SHOES_PLATE_SET2',
+  },
+  {
+    key: 'T7_SHOES_PLATE_SET2',
+    text: "Grandmaster's Knight Shoes",
+    value: 'T7_SHOES_PLATE_SET2',
+  },
+  {
+    key: 'T4_SHOES_PLATE_SET3',
+    text: "Adept's Guardian Shoes",
+    value: 'T4_SHOES_PLATE_SET3',
+  },
+  {
+    key: 'T5_SHOES_PLATE_SET3',
+    text: "Expert's Guardian Shoes",
+    value: 'T5_SHOES_PLATE_SET3',
+  },
+  {
+    key: 'T6_SHOES_PLATE_SET3',
+    text: "Master's Guardian Shoes",
+    value: 'T6_SHOES_PLATE_SET3',
+  },
+  {
+    key: 'T7_SHOES_PLATE_SET3',
+    text: "Grandmaster's Guardian Shoes",
+    value: 'T7_SHOES_PLATE_SET3',
+  },
+]
