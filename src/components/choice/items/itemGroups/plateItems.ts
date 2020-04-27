0723669143253c71a85e28b9a1ed777457cf1408
daelynj <daelyndbjones@gmail.com>
2020-04-27 16:01:37 -0700

    Add plate items grouping

---
+++ src/components/choice/items/itemGroups/plateItems.ts
@@ -0,0 +1,5 @@
+import { helmets } from '../plate/helmets'
+import { armors } from '../plate/armors'
+import { boots } from '../plate/boots'
+
+export const plateItems = helmets.concat(armors, boots)
