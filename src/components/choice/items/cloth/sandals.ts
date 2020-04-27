f248e7c727736ca04aaa858b69047dd9e767079f
daelynj <daelyndbjones@gmail.com>
2020-04-27 16:00:27 -0700

    Add cloth sandals

---
+++ src/components/choice/items/cloth/sandals.ts
@@ -0,0 +1,63 @@
+export const sandals = [
+  {
+    key: 'T4_SHOES_CLOTH_SET1',
+    text: "Adept's Scholar Sandals",
+    value: 'T4_SHOES_CLOTH_SET1',
+  },
+  {
+    key: 'T5_SHOES_CLOTH_SET1',
+    text: "Expert's Scholar Sandals",
+    value: 'T5_SHOES_CLOTH_SET1',
+  },
+  {
+    key: 'T6_SHOES_CLOTH_SET1',
+    text: "Master's Scholar Sandals",
+    value: 'T6_SHOES_CLOTH_SET1',
+  },
+  {
+    key: 'T7_SHOES_CLOTH_SET1',
+    text: "Grandmaster's Scholar Sandals",
+    value: 'T7_SHOES_CLOTH_SET1',
+  },
+
+  {
+    key: 'T4_SHOES_CLOTH_SET2',
+    text: "Adept's Cleric Sandals",
+    value: 'T4_SHOES_CLOTH_SET2',
+  },
+  {
+    key: 'T5_SHOES_CLOTH_SET2',
+    text: "Expert's Cleric Sandals",
+    value: 'T5_SHOES_CLOTH_SET2',
+  },
+  {
+    key: 'T6_SHOES_CLOTH_SET2',
+    text: "Master's Cleric Sandals",
+    value: 'T6_SHOES_CLOTH_SET2',
+  },
+  {
+    key: 'T7_SHOES_CLOTH_SET2',
+    text: "Grandmaster's Cleric Sandals",
+    value: 'T7_SHOES_CLOTH_SET2',
+  },
+  {
+    key: 'T4_SHOES_CLOTH_SET3',
+    text: "Adept's Mage Sandals",
+    value: 'T4_SHOES_CLOTH_SET3',
+  },
+  {
+    key: 'T5_SHOES_CLOTH_SET3',
+    text: "Expert's Mage Sandals",
+    value: 'T5_SHOES_CLOTH_SET3',
+  },
+  {
+    key: 'T6_SHOES_CLOTH_SET3',
+    text: "Master's Mage Sandals",
+    value: 'T6_SHOES_CLOTH_SET3',
+  },
+  {
+    key: 'T7_SHOES_CLOTH_SET3',
+    text: "Grandmaster's Mage Sandals",
+    value: 'T7_SHOES_CLOTH_SET3',
+  },
+]
