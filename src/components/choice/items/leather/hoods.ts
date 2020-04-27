bb9b3190014d39ace222b0a36959a558509662bb
daelynj <daelyndbjones@gmail.com>
2020-04-27 16:00:09 -0700

    Add leather hoods

---
+++ src/components/choice/items/leather/hoods.ts
@@ -0,0 +1,63 @@
+export const hoods = [
+  {
+    key: 'T4_HEAD_LEATHER_SET1',
+    text: "Adept's Mercenary Hood",
+    value: 'T4_HEAD_LEATHER_SET1',
+  },
+  {
+    key: 'T5_HEAD_LEATHER_SET1',
+    text: "Expert's Mercenary Hood",
+    value: 'T5_HEAD_LEATHER_SET1',
+  },
+  {
+    key: 'T6_HEAD_LEATHER_SET1',
+    text: "Master's Mercenary Hood",
+    value: 'T6_HEAD_LEATHER_SET1',
+  },
+  {
+    key: 'T7_HEAD_LEATHER_SET1',
+    text: "Grandmaster's Mercenary Hood",
+    value: 'T7_HEAD_LEATHER_SET1',
+  },
+
+  {
+    key: 'T4_HEAD_LEATHER_SET2',
+    text: "Adept's Hunter Hood",
+    value: 'T4_HEAD_LEATHER_SET2',
+  },
+  {
+    key: 'T5_HEAD_LEATHER_SET2',
+    text: "Expert's Hunter Hood",
+    value: 'T5_HEAD_LEATHER_SET2',
+  },
+  {
+    key: 'T6_HEAD_LEATHER_SET2',
+    text: "Master's Hunter Hood",
+    value: 'T6_HEAD_LEATHER_SET2',
+  },
+  {
+    key: 'T7_HEAD_LEATHER_SET2',
+    text: "Grandmaster's Hunter Hood",
+    value: 'T7_HEAD_LEATHER_SET2',
+  },
+  {
+    key: 'T4_HEAD_LEATHER_SET3',
+    text: "Adept's Assassin Hood",
+    value: 'T4_HEAD_LEATHER_SET3',
+  },
+  {
+    key: 'T5_HEAD_LEATHER_SET3',
+    text: "Expert's Assassin Hood",
+    value: 'T5_HEAD_LEATHER_SET3',
+  },
+  {
+    key: 'T6_HEAD_LEATHER_SET3',
+    text: "Master's Assassin Hood",
+    value: 'T6_HEAD_LEATHER_SET3',
+  },
+  {
+    key: 'T7_HEAD_LEATHER_SET3',
+    text: "Grandmaster's Assassin Hood",
+    value: 'T7_HEAD_LEATHER_SET3',
+  },
+]
