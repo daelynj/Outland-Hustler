e8e1908414346c236b1d671db8601216971c3408
daelynj <daelyndbjones@gmail.com>
2020-04-27 15:59:41 -0700

    Add leather shoes

---
+++ src/components/choice/items/leather/shoes.ts
@@ -0,0 +1,63 @@
+export const shoes = [
+  {
+    key: 'T4_SHOES_LEATHER_SET1',
+    text: "Adept's Mercenary Shoes",
+    value: 'T4_SHOES_LEATHER_SET1',
+  },
+  {
+    key: 'T5_SHOES_LEATHER_SET1',
+    text: "Expert's Mercenary Shoes",
+    value: 'T5_SHOES_LEATHER_SET1',
+  },
+  {
+    key: 'T6_SHOES_LEATHER_SET1',
+    text: "Master's Mercenary Shoes",
+    value: 'T6_SHOES_LEATHER_SET1',
+  },
+  {
+    key: 'T7_SHOES_LEATHER_SET1',
+    text: "Grandmaster's Mercenary Shoes",
+    value: 'T7_SHOES_LEATHER_SET1',
+  },
+
+  {
+    key: 'T4_SHOES_LEATHER_SET2',
+    text: "Adept's Hunter Shoes",
+    value: 'T4_SHOES_LEATHER_SET2',
+  },
+  {
+    key: 'T5_SHOES_LEATHER_SET2',
+    text: "Expert's Hunter Shoes",
+    value: 'T5_SHOES_LEATHER_SET2',
+  },
+  {
+    key: 'T6_SHOES_LEATHER_SET2',
+    text: "Master's Hunter Shoes",
+    value: 'T6_SHOES_LEATHER_SET2',
+  },
+  {
+    key: 'T7_SHOES_LEATHER_SET2',
+    text: "Grandmaster's Hunter Shoes",
+    value: 'T7_SHOES_LEATHER_SET2',
+  },
+  {
+    key: 'T4_SHOES_LEATHER_SET3',
+    text: "Adept's Assassin Shoes",
+    value: 'T4_SHOES_LEATHER_SET3',
+  },
+  {
+    key: 'T5_SHOES_LEATHER_SET3',
+    text: "Expert's Assassin Shoes",
+    value: 'T5_SHOES_LEATHER_SET3',
+  },
+  {
+    key: 'T6_SHOES_LEATHER_SET3',
+    text: "Master's Assassin Shoes",
+    value: 'T6_SHOES_LEATHER_SET3',
+  },
+  {
+    key: 'T7_SHOES_LEATHER_SET3',
+    text: "Grandmaster's Assassin Shoes",
+    value: 'T7_SHOES_LEATHER_SET3',
+  },
+]
