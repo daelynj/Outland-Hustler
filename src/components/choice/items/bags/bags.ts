d382a7eeae1f8b04c7617f004a333031643d0312
daelynj <daelyndbjones@gmail.com>
2020-04-27 16:00:57 -0700

    Add bags

---
+++ src/components/choice/items/bags/bags.ts
@@ -0,0 +1,22 @@
+export const bags = [
+  {
+    key: 'T4_BAG',
+    text: "Adept's Bag",
+    value: 'T4_BAG',
+  },
+  {
+    key: 'T5_BAG',
+    text: "Expert's Bag",
+    value: 'T5_BAG',
+  },
+  {
+    key: 'T6_BAG',
+    text: "Master's Bag",
+    value: 'T6_BAG',
+  },
+  {
+    key: 'T7_BAG',
+    text: "Grandmaster's Bag",
+    value: 'T7_BAG',
+  },
+]
