6a2bf47c98b50647c1f4789bd74aea642b9fd3bc
daelynj <daelyndbjones@gmail.com>
2020-04-27 20:33:54 -0700

    Add cloth robes enchantment

--- src/components/choice/items/cloth/robes.ts
+++ src/components/choice/items/cloth/robes.ts
@@ -19,7 +19,6 @@ export const robes = [
     text: "Grandmaster's Scholar Robe",
     value: 'T7_ARMOR_CLOTH_SET1',
   },
-
   {
     key: 'T4_ARMOR_CLOTH_SET2',
     text: "Adept's Cleric Robe",
@@ -59,5 +58,65 @@ export const robes = [
     key: 'T7_ARMOR_CLOTH_SET3',
     text: "Grandmaster's Mage Robe",
     value: 'T7_ARMOR_CLOTH_SET3',
+  },
+  {
+    key: 'T4_ARMOR_CLOTH_SET1@1',
+    text: "Adept's Scholar Robe@1",
+    value: 'T4_ARMOR_CLOTH_SET1@1',
+  },
+  {
+    key: 'T5_ARMOR_CLOTH_SET1@1',
+    text: "Expert's Scholar Robe@1",
+    value: 'T5_ARMOR_CLOTH_SET1@1',
+  },
+  {
+    key: 'T6_ARMOR_CLOTH_SET1@1',
+    text: "Master's Scholar Robe@1",
+    value: 'T6_ARMOR_CLOTH_SET1@1',
+  },
+  {
+    key: 'T7_ARMOR_CLOTH_SET1@1',
+    text: "Grandmaster's Scholar Robe@1",
+    value: 'T7_ARMOR_CLOTH_SET1@1',
+  },
+  {
+    key: 'T4_ARMOR_CLOTH_SET2@1',
+    text: "Adept's Cleric Robe@1",
+    value: 'T4_ARMOR_CLOTH_SET2@1',
+  },
+  {
+    key: 'T5_ARMOR_CLOTH_SET2@1',
+    text: "Expert's Cleric Robe@1",
+    value: 'T5_ARMOR_CLOTH_SET2@1',
+  },
+  {
+    key: 'T6_ARMOR_CLOTH_SET2@1',
+    text: "Master's Cleric Robe@1",
+    value: 'T6_ARMOR_CLOTH_SET2@1',
+  },
+  {
+    key: 'T7_ARMOR_CLOTH_SET2@1',
+    text: "Grandmaster's Cleric Robe@1",
+    value: 'T7_ARMOR_CLOTH_SET2@1',
+  },
+  {
+    key: 'T4_ARMOR_CLOTH_SET3@1',
+    text: "Adept's Mage Robe@1",
+    value: 'T4_ARMOR_CLOTH_SET3@1',
+  },
+  {
+    key: 'T5_ARMOR_CLOTH_SET3@1',
+    text: "Expert's Mage Robe@1",
+    value: 'T5_ARMOR_CLOTH_SET3@1',
+  },
+  {
+    key: 'T6_ARMOR_CLOTH_SET3@1',
+    text: "Master's Mage Robe@1",
+    value: 'T6_ARMOR_CLOTH_SET3@1',
+  },
+  {
+    key: 'T7_ARMOR_CLOTH_SET3@1',
+    text: "Grandmaster's Mage Robe@1",
+    value: 'T7_ARMOR_CLOTH_SET3@1',
   },
 ]
